e4a111107576c7fc65dda87e03578ec842f780d0
Yoona Hwang <younalovem@gmail.com>
2025-07-04 10:49:50 +0900

    Update AbstractEntity.cs

--- Assets/Script/Entity/AbstractEntity.cs
+++ Assets/Script/Entity/AbstractEntity.cs
@@ -1,13 +1,16 @@
 using UnityEngine;
 
-public abstract class AbstractEntity : MonoBehaviour {
+public abstract class AbstractEntity : MonoBehaviour
+{
     [Header("Entity's life info")]
-    [SerializeField] protected float HP;
+    [SerializeField] protected float maxHP;
+    [SerializeField] protected float currentHP;
     [SerializeField] protected float attackRate;
 
     [Header("Movement System")]
     [SerializeField] protected float walkSpeed;
     [SerializeField] protected float runSpeed;
+    [SerializeField] protected bool facingLeft;
 
     [Header("Ground Check")]
     [SerializeField] protected float groundCheckDistance = 0.1f;
@@ -15,7 +18,23 @@ public abstract class AbstractEntity : MonoBehaviour {
     [SerializeField] protected LayerMask groundLayer;
     protected bool isGrounded;
 
+    [Header("Wall Check")]
+    [SerializeField] protected float wallCheckDistance;
+    [SerializeField] protected Transform wallCheckerTransform;
+    [SerializeField] protected LayerMask wallLayer;
+    protected bool isWall;
+
+    [Header("knockback")]
+    [SerializeField] protected Vector2 knockbackForce;
+    [SerializeField] protected float knockbackDuration;
+    [SerializeField] protected bool isknockBack;
+    protected int knockbackDir; // -1 or 1
+
     public abstract void Attack();
     protected abstract void Move();
+
+    // public abstract void TakeDamage(float damage);
+    // protected abstract void Die();
+    // protected abstract void DoKnockback();
 
 }
