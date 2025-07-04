6171e8088b1de6d57fbf22f677fe8ccb4ce5feda
kimgunwoocode <105213943+kimgunwoocode@users.noreply.github.com>
2025-07-04 13:54:01 +0900

    map platforms

---
+++ Assets/Script/Map/Interation.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
+
+public class Interation : MonoBehaviour
+{
+    [Header("상호작용시 실행할 이벤트 (메서드)")]
+    public UnityEvent InterationEvent;
+    [Space]
+    public GameObject InteractionGuide;
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        
+    }
+}
