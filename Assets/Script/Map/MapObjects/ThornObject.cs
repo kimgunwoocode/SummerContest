6c791849810b458051843ba49be2777b37774942
kimgunwoocode <105213943+kimgunwoocode@users.noreply.github.com>
2025-07-11 02:00:08 +0900

    thonObject
    나중에 이어서...

---
+++ Assets/Script/Map/MapObjects/ThornObject.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ThornObject : MonoBehaviour
+{
+    public List<Vector2> ThornSpawnPoint_Position;
+
+    // 플레이어 공격 함수 가져다 쓰기 (엔티티 그거)
+    // 그 다음 가장 가까운 ThornSpawnPoint 위치로 이동시키기
+}
