f8ecefd34f0c9673569a5e636b2cd51ea8034a31
bbeak-serif <bgh9389@gmail.com>
2025-07-19 02:39:35 +0900

    Revert "shop"
    This reverts commit eebe6bfc8fa024fc51dfdc66410b943c83e1df05.

--- Assets/Script/Map/SavePoint/SavePoint.cs
+++ Assets/Script/Map/SavePoint/SavePoint.cs
@@ -6,14 +6,14 @@ public class SavePoint : MonoBehaviour
     [Header("type option")]
     public SP_type SavePoint_type;
     [Header("ID")]
-    public int ID;
+    public int SavePoint_ID;
     [Space]
     [Header("is activate")]
     public bool SavePointEnabled;
     
     public void InteractSavePoint()
     {
-        print("SavePoint_"+SavePoint_type+" ID:"+ID);
+        print("SavePoint_"+SavePoint_type+" ID:"+SavePoint_ID);
         Singleton.GameManager_Instance.Get<GameManager>().SaveData__SavePoint();
     }
 }
