cc58bf7d75cc0a24c0ea41a09ed1144480252e34
kimgunwoocode <105213943+kimgunwoocode@users.noreply.github.com>
2025-07-19 23:28:44 +0900

    playerdata

--- Assets/Script/Entity/Player/PlayerData.cs
+++ Assets/Script/Entity/Player/PlayerData.cs
@@ -1,17 +1,17 @@
-using System.Collections.Generic;
+ï»¿using System.Collections.Generic;
 
 [System.Serializable]
 public class PlayerData
 {
-    public int MaxHP; // ÇÃ·¹ÀÌ¾î ÃÖ´ë Ã¼·Â
-    public int CurrentHP; // ÇÃ·¹ÀÌ¾î ÇöÀç Ã¼·Â
+    public int MaxHP; // í”Œë ˆì´ì–´ ìµœëŒ€ ì²´ë ¥
+    public int CurrentHP; // í”Œë ˆì´ì–´ í˜„ì¬ ì²´ë ¥
     public int ATK;
-    public float MaxBreathGauge; // ÃÖ´ë ºê·¹½º °ÔÀÌÁö
-    public float CurrentBreathGauge; // ºê·¹½º °ÔÀÌÁö
-    public int Money; // º¸À¯ÁßÀÎ µ·
-    public List<int> EquipSkill; // ÀåÂøÁßÀÎ ½ºÅ³
+    public float MaxBreathGauge; // ìµœëŒ€ ë¸Œë ˆìŠ¤ ê²Œì´ì§€
+    public float CurrentBreathGauge; // ë¸Œë ˆìŠ¤ ê²Œì´ì§€
+    public int Money; // ë³´ìœ ì¤‘ì¸ ëˆ
+    public List<int> EquipSkill; // ì¥ì°©ì¤‘ì¸ ìŠ¤í‚¬
 
 
-    public Dictionary<int, bool> PlayerSkill = new(); // ÇØ±İµÈ ÇÃ·¹ÀÌ¾î ½ºÅ³ <½ºÅ³ID, ÇØ±İ ¿©ºÎ>
-    public Dictionary<int, bool> GettedItems = new(); // º¸À¯ÁßÀÎ ¾ÆÀÌÅÛ
+    public Dictionary<int, bool> PlayerSkill = new(); // í•´ê¸ˆëœ í”Œë ˆì´ì–´ ìŠ¤í‚¬ <ìŠ¤í‚¬ID, í•´ê¸ˆ ì—¬ë¶€>
+    public Dictionary<int, bool> GettedItems = new(); // ë³´ìœ ì¤‘ì¸ ì•„ì´í…œ
 }
