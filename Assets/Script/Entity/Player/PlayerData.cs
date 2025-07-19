3bfed1c2fd7d856400b30ee1cfdc9092a05618c0
kimgunwoocode <105213943+kimgunwoocode@users.noreply.github.com>
2025-07-19 23:32:21 +0900

    brench-gamemanager

--- Assets/Script/Entity/Player/PlayerData.cs
+++ Assets/Script/Entity/Player/PlayerData.cs
@@ -11,7 +11,16 @@ public class PlayerData
     public int Money; // 보유중인 돈
     public List<int> EquipSkill; // 장착중인 스킬
 
-
+    /// <summary>
+    /// ID. 해금되는 기능
+    /// 0. 돌진
+    /// 1. 브레스
+    /// 2. 이단 점프
+    /// 3. 낙하공격
+    /// 4. 활공
+    /// 5. 벽타기
+    /// </summary>
+    public List<bool> PlayerAbility = new();// 플레이어 기능 해금 여부
     public Dictionary<int, bool> PlayerSkill = new(); // 해금된 플레이어 스킬 <스킬ID, 해금 여부>
     public Dictionary<int, bool> GettedItems = new(); // 보유중인 아이템
 }
