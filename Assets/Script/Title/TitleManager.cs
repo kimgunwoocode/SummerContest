a9f1469efbe259c0244433fbc9eb5e2ece4c1ea4
kimgunwoocode <105213943+kimgunwoocode@users.noreply.github.com>
2025-07-30 05:20:16 +0900

    ...

--- Assets/Script/Title/TitleManager.cs
+++ Assets/Script/Title/TitleManager.cs
@@ -1,32 +1,42 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+using System.Collections;
 
 public class TitleManager : MonoBehaviour
 {
     public string StartSceneName;
     [Space]
     public TextAsset InitData;
+    public TextAsset SavePointID_json;
     public GameManager GameManager;
     public GameDataManager GameDataManager;
 
+    Dictionary<int, string> SavePointID_list = new();
 
 
     private void Awake()
     {
         GameManager = Singleton.GameManager_Instance.Get<GameManager>();
         GameDataManager = Singleton.GameManager_Instance.Get<GameDataManager>();
+
+        SavePointID_list = MapTool.DictionaryFromJson(SavePointID_json.text);
     }
 
 
     public void ContinueSavedGame(int index)
     {
         SaveFileManager.Load(index);
+        StartSceneName = SavePointID_list[GameDataManager.SpawnPoint];
+        GameManager.CurrentScenePointID = -GameDataManager.SpawnPoint;
+        SceneManager.LoadScene(StartSceneName);
     }
 
     public void StartNewGame()
     {
-        //GameDataManager.LoadGameData(InitData.(InitData));
-        GameManager.LoadData__SavePoint();
+        SaveFileManager.Load(0);
+        StartSceneName = "1-1_ForgottenNest";
+        GameManager.CurrentScenePointID = -1;
         SceneManager.LoadScene(StartSceneName);
     }
 }
--- Assets/Script/Title/TitleUI_Manager.cs
+++ Assets/Script/Title/TitleUI_Manager.cs
@@ -6,16 +6,19 @@ public class TitleUI_Manager : MonoBehaviour
 
 
 
-
+    private void Start()
+    {
+        SelectPanel_Screen.SetActive(false);
+    }
 
 
     public void Open_SelectPanel_Screen()
     {
-
+        SelectPanel_Screen.SetActive(true);
     }
 
     public void Closs_SelectPanel_Screen()
     {
-
+        SelectPanel_Screen.SetActive(false);
     }
 }
