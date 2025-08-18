d83f6c9fc65ca08eb1eaac60d59158174c11a582
bbeak-serif <bgh9389@gmail.com>
2025-08-18 15:04:54 +0900

    burnWall

--- Assets/Script/Map/Ground/BurnWall.cs
+++ Assets/Script/Map/Ground/BurnWall.cs
@@ -1,9 +1,11 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class BurnWall : EnemyEntity
 {
     GameDataManager gameDataManager;
     Interaction interaction;
+    Material burnWallMat;
 
     public override void TakeDamage(int damageAmount, Vector2 attackerPosition, AttackType attackType)
     {
@@ -11,8 +13,22 @@ public class BurnWall : EnemyEntity
         {
             // 애니메이션, 사운드 추가
             gameDataManager.InteractionObjects[interaction.ID] = false;
-            gameObject.SetActive(false);
+            StartCoroutine(Burn(1.2f));
         }
+    }
+    private IEnumerator Burn(float duration) {
+        float ET = 0;
+        float startX = 0;
+        float endX = 1.1f;
+        while (ET < duration) {
+            ET += Time.deltaTime;
+            float newDissolve = Mathf.Lerp(startX, endX, ET/duration);
+            burnWallMat.SetFloat("_Dissolve", newDissolve);
+            yield return null;
+        }
+
+        burnWallMat.SetFloat("_Dissolve", endX);
+        gameObject.SetActive(false);
     }
 
 
@@ -21,13 +37,14 @@ public class BurnWall : EnemyEntity
     {
         gameDataManager = Singleton.GameManager_Instance.Get<GameDataManager>();
         interaction = gameObject.GetComponent<Interaction>();
+        burnWallMat = gameObject.GetComponent<SpriteRenderer>().material;
     }
     protected override void Start() { }
     protected override void Update() { }
     protected override void FixedUpdate() { }
     public override void OnDrawGizmos() { }
     #endregion
-    public void init()
+    public void Init()
     {
         Destroy(gameObject);
     }
