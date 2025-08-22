521e2e172f2fa2250818ea47b29b03856070204b
kimgunwoocode <105213943+kimgunwoocode@users.noreply.github.com>
2025-08-22 23:41:18 +0900

    .

---
+++ Assets/Script/Map/Ground/Editor/BurnWallEditor.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor;
+
+[CustomEditor(typeof(BurnWall))]
+public class BurnWallEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        // 내가 정의한 필드만 수동으로 보여줌
+        //EditorGUILayout.PropertyField(serializedObject.FindProperty("변수 이름"));
+
+        serializedObject.ApplyModifiedProperties();
+    }
+}
